d24ecab6265453a84ef03dc8ad21adcf744b0d75
Marco Terrinoni <marco.terrinoni90@gmail.com>
2016-03-23 22:39:56 +0100

    code inclusion

---
+++ app/src/main/java/it/terrinoni/justjava/MainActivity.java
@@ -0,0 +1,71 @@
+package it.terrinoni.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    private int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+//        String message = "Total: " + (quantity * 5) + " €";
+//        message += "\nThank you";
+        String msg = createOrderSummary(5);
+        displayMessage(msg);
+    }
+
+    public void increment(View view) {
+        display(++quantity);
+    }
+
+    public void decrement(View view) {
+        display(--quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    private String createOrderSummary(int price) {
+        String msg = "Name: Captain Terry\nQuantity: " + String.valueOf(quantity) + "\nTotal: " +
+                (quantity * price) + " €\nThank you";
+        return msg;
+    }
+}
