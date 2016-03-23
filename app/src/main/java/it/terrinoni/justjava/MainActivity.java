47f21d2b3e719fa550dcccf57ff84f406d328d9d
Marco Terrinoni <marco.terrinoni90@gmail.com>
2016-03-23 22:50:23 +0100

    change price to order summary

--- app/src/main/java/it/terrinoni/justjava/MainActivity.java
+++ app/src/main/java/it/terrinoni/justjava/MainActivity.java
@@ -24,8 +24,6 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-//        String message = "Total: " + (quantity * 5) + " €";
-//        message += "\nThank you";
         String msg = createOrderSummary(5);
         displayMessage(msg);
     }
@@ -42,25 +40,16 @@ public class MainActivity extends ActionBarActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     private String createOrderSummary(int price) {
